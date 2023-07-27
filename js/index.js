f46432a1a0ff733bd017b35b90ef972ebf275416
Antony Praveenkumar M <antony.praveenkumar@softsuave.com>
2023-07-27 18:48:33 +0530

    Js Added

---
+++ js/index.js
@@ -0,0 +1,7 @@
+fetch("./templates/navBar.html")
+  .then((response) => response.text())
+  .then((content) => {
+    const navBarContainer = document.getElementById("navBarContainer");
+    navBarContainer.innerHTML = content;
+  })
+  .catch((error) => console.error("Error fetching two.html:", error));
